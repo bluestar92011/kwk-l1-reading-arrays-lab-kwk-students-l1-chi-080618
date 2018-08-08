971c3c36fa5a38cfdd2fa75b2c3669f70ac5b412
bluestar92011 <bluestar92011@gmail.com>
2018-08-08 19:11:16 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,24 +9,46 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[0]
 end
+
+first_student_by_index
+
+
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
-end
+#   # Write a solution that returns the first student in the array student_names
+ STUDENT_NAMES[3]
+ end
+ puts fourth_student_by_index
+ 
+ fourth_student_by_index
 
-def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+ def last_student_by_index
+#   # Write a solution that returns the first student in the array student_names
+STUDENT_NAMES[4]
 end
+puts last_student_by_index
+
+last_student_by_index
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+#   # Write a solution that returns the first student using the built in .first method
+STUDENT_NAMES.first
 end
+puts first_student_by_method
 
-def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+first_student_by_method
+
+
+ def last_student_by_method
+#   # Write a solution that returns the first student using the built in .last method
+STUDENT_NAMES.last
 end
+puts last_student_by_method
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+#   # Write a solution that returns the first, second and third students
+  STUDENT_NAMES[0..3]
 end
+puts first_second_and_third_students
